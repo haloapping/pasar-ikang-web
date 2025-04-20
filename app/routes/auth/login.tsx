245b6ec27640550a29df120263db54313e25ff13
haloapping <alfiyanto.kondolele@gmail.com>
2025-04-20 23:13:09 +0700

    fix: register and login form

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -2,18 +2,11 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import type { z } from "zod";
 import { Button } from "~/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/ui/form";
 import { Input } from "~/components/ui/input";
 import { LoginFormSchema } from "~/types/auth/login";
 import type { Route } from "./+types/register";
+import { Form, NavLink } from "react-router";
+import { Label } from "~/components/ui/label";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -23,7 +16,11 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Login() {
-  const form = useForm<z.infer<typeof LoginFormSchema>>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof LoginFormSchema>>({
     resolver: zodResolver(LoginFormSchema),
     defaultValues: {
       username: "",
@@ -31,51 +28,46 @@ export default function Login() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof LoginFormSchema>) {
-    console.log(values);
-  }
+  const onSubmit = () => {};
 
   return (
     <div className="mt-14 flex justify-center">
       <div className="w-2xl">
         <h1 className="mb-5 text-4xl">Login</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <>
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </>
-              )}
-            />
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <>
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                </>
-              )}
-            />
+        <Form
+          method="POST"
+          onSubmit={handleSubmit(onSubmit)}
+          className="mb-3 flex flex-col gap-4"
+        >
+          <div>
+            <Label htmlFor="username" className="mb-2">
+              Username
+            </Label>
+            <Input type="text" id="username" {...register("username")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.username?.message}
+            </p>
+          </div>
 
-            <Button type="submit">Login</Button>
-          </form>
+          <div>
+            <Label htmlFor="password" className="mb-2">
+              Password
+            </Label>
+            <Input type="text" id="password" {...register("password")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.password?.message}
+            </p>
+          </div>
+
+          <Button type="submit">Login</Button>
         </Form>
+        <p className="text-center">
+          Don't have an account?{" "}
+          <NavLink to={"/register"} className="underline underline-offset-2">
+            Register
+          </NavLink>
+        </p>
       </div>
     </div>
   );
--- app/routes/auth/register.tsx
+++ app/routes/auth/register.tsx
@@ -1,19 +1,13 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, type UseFormRegister } from "react-hook-form";
 import type { z } from "zod";
-import { Button } from "~/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/ui/form";
-import { Input } from "~/components/ui/input";
-import { RegisterFormSchema } from "~/types/auth/register";
+
+import { RegisterFormSchema, type RegisterForm } from "~/types/auth/register";
 import type { Route } from "./+types/register";
+import { Form, redirect } from "react-router";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -22,10 +16,46 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Register() {
-  const form = useForm<z.infer<typeof RegisterFormSchema>>({
+export async function action({ request }: Route.ActionArgs) {
+  const registerFormData = await request.formData();
+  console.log(registerFormData);
+
+  const newUser: RegisterForm = {
+    firstName: String(registerFormData.get("firstName")),
+    lastName: String(registerFormData.get("lastName")),
+    username: String(registerFormData.get("username")),
+    email: String(registerFormData.get("email")),
+    password: String(registerFormData.get("password")),
+    confirmPassword: String(registerFormData.get("confirmPassword")),
+  };
+
+  const response = await fetch(`${process.env.BACKEND_API_URL}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newUser),
+  });
+
+  const registerResult = await response.json();
+
+  console.dir(registerResult, { depth: null });
+
+  if (!response.ok) {
+    return redirect("/register");
+  }
+
+  return redirect("/login");
+}
+
+export default function Register({ actionData }: Route.ComponentProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof RegisterFormSchema>>({
     resolver: zodResolver(RegisterFormSchema),
     defaultValues: {
+      firstName: "",
+      lastName: "",
       username: "",
       email: "",
       password: "",
@@ -33,74 +63,76 @@ export default function Register() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof RegisterFormSchema>) {
-    console.log(values);
-  }
+  const onSubmit = () => {};
 
   return (
     <div className="mt-14 flex justify-center">
       <div className="w-2xl">
         <h1 className="mb-5 text-4xl">Register</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+        <Form
+          method="POST"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <div>
+            <Label htmlFor="firstName" className="mb-2">
+              First Name
+            </Label>
+            <Input type="text" id="firstName" {...register("firstName")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.firstName?.message}
+            </p>
+          </div>
 
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <div>
+            <Label htmlFor="lastName" className="mb-2">
+              Last Name
+            </Label>
+            <Input type="text" id="lastName" {...register("lastName")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.lastName?.message}
+            </p>
+          </div>
 
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <div>
+            <Label htmlFor="username" className="mb-2">
+              Username
+            </Label>
+            <Input type="text" id="username" {...register("username")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.username?.message}
+            </p>
+          </div>
 
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+          <div>
+            <Label htmlFor="email" className="mb-2">
+              Email
+            </Label>
+            <Input type="text" id="email" {...register("email")} />
+            <p className="mt-1 text-sm text-red-400">{errors.email?.message}</p>
+          </div>
 
-            <Button type="submit">Register</Button>
-          </form>
+          <div>
+            <Label htmlFor="password" className="mb-2">
+              Password
+            </Label>
+            <Input type="password" {...register("password")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.password?.message}
+            </p>
+          </div>
+
+          <div>
+            <Label htmlFor="confirm-password" className="mb-2">
+              Confirm Password
+            </Label>
+            <Input type="password" {...register("confirmPassword")} />
+            <p className="mt-1 text-sm text-red-400">
+              {errors.confirmPassword?.message}
+            </p>
+          </div>
+
+          <Button type="submit">Register</Button>
         </Form>
       </div>
     </div>
