b848ede214592d0db2948f3fe0779d3e4e758f6f
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-26 18:48:03 +0700

    feat: product loader & action only in route

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -1,6 +1,9 @@
 import ProductSlug from "~/components/product/product-slug";
 import type { Product } from "~/types/product";
 import type { Route } from "./+types/home";
+import { destroySession, getSession } from "~/session.server";
+import { redirect } from "react-router";
+import type { AddToCartType } from "~/types/cart";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -21,6 +24,38 @@ export async function loader({ params }: Route.ActionArgs) {
   } catch (error) {
     return error;
   }
+}
+
+export async function action({ request }: Route.ActionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  if (!session.has("token")) {
+    return redirect("/login");
+  }
+  const token = session.get("token");
+
+  const formData = await request.formData();
+
+  const addCartItemData: AddToCartType = {
+    productId: String(formData.get("productId")),
+    quantity: Number(formData.get("quantity")),
+  };
+
+  const response = await fetch(`${process.env.BACKEND_API_URL}/cart/items`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(addCartItemData),
+  });
+  if (!response.ok) {
+    session.flash("error", "Failed to add item to cart");
+    return redirect("/login", {
+      headers: { "Set-Cookie": await destroySession(session) },
+    });
+  }
+
+  return redirect("/cart");
 }
 
 export default function Product({ loaderData }: Route.ComponentProps) {
